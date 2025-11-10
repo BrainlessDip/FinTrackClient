ab01a1222ff9f6b9efe826a990fe1720e9d23044
Dip Dey <dipdey993@gmail.com>
2025-11-10 19:18:54 +0600

    feat: add transaction page

--- src/pages/AddTransaction.jsx
+++ src/pages/AddTransaction.jsx
@@ -1,21 +1,137 @@
-import React from "react";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import useAuth from "../hooks/useAuth";
 
 const AddTransaction = () => {
+  const { user } = useAuth();
+
+  const [costType, setCostType] = useState("");
+  const handleAddTransaction = (e) => {
+    e.preventDefault();
+    const type = e.target.type.value;
+    const category = e.target.category.value;
+    const amount = e.target.amount.value;
+    const description = e.target.description.value;
+    const date = e.target.date.value;
+    const email = e.target.email.value;
+    const name = e.target.name.value;
+
+    console.log({
+      type,
+      category,
+      amount,
+      description,
+      date,
+      email,
+      name,
+    });
+
+    if (!["income", "expense"].includes(type)) {
+      toast.error("Please select a valid type: Income or Expense");
+      return;
+    }
+
+    if (category == "Select a category") {
+      toast.error("Please select a valid category");
+      return;
+    }
+  };
   return (
-    <div>
+    <div className="bg-base-300">
+      <h1 className="mx-auto text-4xl pt-5 border-b-2 border-blue-500 w-fit">
+        Add Transaction
+      </h1>
       <div className="flex justify-center items-center flex-col relative min-h-[calc(100vh-64px)] md:min-h-[calc(100vh-74px)]">
-        <h1 className="text-4xl border-b-2 border-blue-500">Add Transaction</h1>
         <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-          <form>
-            <label className="label">Email</label>
+          <form onSubmit={handleAddTransaction} className="space-y-2">
+            <select
+              defaultValue="Select a cost type"
+              className="select"
+              name="type"
+              required
+              onChange={(e) => {
+                const type = e.target.value;
+                setCostType(type);
+                if (costType !== type) {
+                  e.target.parentNode.category.value = "Select a category";
+                }
+              }}
+            >
+              <option disabled={true}>Select a cost type</option>
+              <option value="income">Income</option>
+              <option value="expense">Expense</option>
+            </select>
+
+            <select
+              defaultValue="Select a category"
+              className="select"
+              name="category"
+              required
+            >
+              <option disabled={true}>Select a category</option>
+              <optgroup label="Income" hidden={costType !== "income"}>
+                <option value="salary">Salary</option>
+                <option value="freelance">Freelance</option>
+                <option value="investments">Investments</option>
+                <option value="bonus">Bonus</option>
+                <option value="gifts">Gifts</option>
+                <option value="other_income">Other</option>
+              </optgroup>
+
+              <optgroup label="Expenses" hidden={costType !== "expense"}>
+                <option value="food">Food</option>
+                <option value="rent">Rent</option>
+                <option value="utilities">Utilities</option>
+                <option value="entertainment">Entertainment</option>
+                <option value="transport">Transport</option>
+                <option value="healthcare">Healthcare</option>
+                <option value="education">Education</option>
+                <option value="shopping">Shopping</option>
+                <option value="subscriptions">Subscriptions</option>
+                <option value="other_expense">Other</option>
+              </optgroup>
+            </select>
+
+            <input
+              type="number"
+              name="amount"
+              placeholder="Type amount"
+              min={1}
+              required
+              className="input"
+            />
+
             <input
               type="text"
-              className="input focus:outline-0"
-              name="email"
-              placeholder="Email"
-              autoComplete="username"
+              name="description"
+              placeholder="Enter description"
+              className="input"
+              autoComplete="off"
               required
             />
+
+            <input
+              type="datetime-local"
+              name="date"
+              required
+              className="input"
+            />
+
+            <input
+              type="email"
+              name="email"
+              defaultValue={user.email}
+              className="input"
+              readOnly
+            />
+            <input
+              type="email"
+              name="name"
+              value={user.displayName || undefined}
+              className="input"
+              readOnly
+            />
+
             <button className="btn btn-neutral mt-4 w-full">
               Add Transaction
             </button>
